603c609473c1b59e29304077cff1a8f4e0f1cb77
Xiaoyue Pan <xyypan@gmail.com>
2018-07-10 22:40:37 +0100

    C++ training materials

--- lambda/code_on_slides/p3.cpp
+++ lambda/code_on_slides/p3.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 #include <vector>
 
-int main () {
-	std::vector < int > a {1 ,2 ,3};
-	//TODO: define print_lambda here
-	//std::for_each will call print_lambda
-	std::for_each (a.begin(), a.end(), print_lambda);
-	return 0;
+int main() {
+  std::vector<int> a{1, 2, 3};
+  // TODO: define print_lambda here
+  // std::for_each will call print_lambda
+  std::for_each(a.begin(), a.end(), print_lambda);
+  return 0;
 }
